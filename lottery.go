5fd914c9709e0a946d5173bc78753fa23f3d09a0
Frank Xu <frank.xu@northbright.com>
2020-01-07 17:24:44 +0800

    Initialize lottery-server

---
+++ lottery.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"path"
+	"sync"
+	"time"
+
+	"github.com/northbright/csvhelper"
+	"github.com/northbright/pathhelper"
+)
+
+var (
+	participantsCSV   = `participants.csv`
+	configFile        = `config.json`
+	config            Config
+	participants      []Participant
+	availParticipants []Participant
+	winners           = map[int][]Participant{}
+	cancelMap         = map[int]context.CancelFunc{}
+	ctxMap            = map[int]context.Context{}
+)
+
+type Participant struct {
+	ID   string
+	Name string
+}
+
+type Prize struct {
+	Name string `json:"name"`
+	Num  int    `json:"num"`
+}
+
+type Blacklist struct {
+	MaxPrizeIndex int      `json:"max_prize_index"`
+	IDs           []string `json:"ids"`
+}
+
+type Config struct {
+	Prizes     []Prize     `json:"prizes"`
+	Blacklists []Blacklist `json:"blacklists"`
+}
+
+type Action struct {
+	Name       string `json:"name"`
+	PrizeIndex int    `json:"prize_index"`
+}
+
+type CommonResponse struct {
+	Success bool   `json:"success"`
+	ErrMsg  string `json:"err_msg"`
+	Action  `json:"action"`
+}
+
+type GetPrizesResponse struct {
+	CommonResponse
+	Prizes []Prize `json:"prizes"`
+}
+
+type StartStopResponse struct {
+	CommonResponse
+	Winners []Participant `json:"winners"`
+}
+
+func loadParticipants(file string) ([]Participant, error) {
+	currentDir, _ := pathhelper.GetCurrentExecDir()
+	file = path.Join(currentDir, file)
+
+	rows, err := csvhelper.ReadFile(file)
+	if err != nil {
+		return []Participant{}, err
+	}
+
+	var participants []Participant
+	for _, row := range rows {
+		if len(row) != 2 {
+			return []Participant{}, fmt.Errorf("incorrect participants CSV")
+		}
+		participants = append(participants, Participant{row[0], row[1]})
+	}
+	return participants, nil
+
+}
+
+func loadConfig(file string, config *Config) error {
+	currentDir, _ := pathhelper.GetCurrentExecDir()
+	file = path.Join(currentDir, file)
+
+	// Load Conifg
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(buf, config)
+}
+
+func sendResponse(c *Client, res interface{}) error {
+	buf, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	c.send <- buf
+	return nil
+}
+
+func parseAction(message []byte) (Action, error) {
+	action := Action{}
+
+	if err := json.Unmarshal(message, &action); err != nil {
+		return action, err
+	}
+	return action, nil
+}
+
+func processAction(c *Client, message []byte) {
+	fmt.Printf("processAction() start, message: %s\n", message)
+
+	mutex := &sync.Mutex{}
+
+	action, err := parseAction(message)
+	if err != nil {
+		fmt.Printf("parseAction() error: %v\n", err)
+		return
+	}
+
+	switch action.Name {
+	case "get_prizes":
+		if err = getPrizes(c, action); err != nil {
+			fmt.Printf("getPrizes() error: %v\n", err)
+		}
+	case "start":
+		if _, ok := cancelMap[action.PrizeIndex]; ok {
+			break
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		cancelMap[action.PrizeIndex] = cancel
+		ctxMap[action.PrizeIndex] = ctx
+		go getWinners(ctx, c, action, mutex)
+
+	case "stop":
+		fmt.Printf("stop")
+		cancel, ok := cancelMap[action.PrizeIndex]
+		if !ok {
+			break
+		}
+		cancel()
+		ctx, ok := ctxMap[action.PrizeIndex]
+		if ok {
+			<-ctx.Done()
+			delete(cancelMap, action.PrizeIndex)
+		}
+	}
+}
+
+func getWinners(ctx context.Context, c *Client, a Action, mutex *sync.Mutex) {
+	var (
+		err     error
+		errMsg  = ""
+		winners []Participant
+	)
+
+	mutex.Lock()
+	defer func() {
+		mutex.Unlock()
+
+		commonRes := CommonResponse{Success: false, ErrMsg: errMsg, Action: a}
+		if errMsg == "" {
+			commonRes.Success = true
+		}
+
+		res := StartStopResponse{commonRes, winners}
+		if err = sendResponse(c, res); err != nil {
+			fmt.Printf("sendResponse() err: %v\n", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("ctx.Done() in getWinners\n")
+			return
+		default:
+		}
+
+		if winners, err = _getWinners(
+			config.Prizes,
+			a.PrizeIndex,
+			availParticipants,
+			config.Blacklists,
+		); err != nil {
+			errMsg = fmt.Sprintf("_getWinners() error: %v", err)
+			fmt.Println(errMsg)
+			return
+		}
+
+		commonRes := CommonResponse{Success: true, ErrMsg: errMsg, Action: a}
+		res := StartStopResponse{commonRes, winners}
+
+		if err = sendResponse(c, res); err != nil {
+			fmt.Printf("sendResponse() err: %v\n", err)
+		}
+
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
+func getPrizes(c *Client, a Action) error {
+	commonRes := CommonResponse{Success: true, ErrMsg: "", Action: a}
+	res := GetPrizesResponse{commonRes, config.Prizes}
+
+	buf, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	c.send <- buf
+	return nil
+}
+
+func getAvailParticipantsForPrize(prizeIndex int, origin []Participant, blacklists []Blacklist) ([]Participant, error) {
+	if len(origin) <= 0 {
+		return origin, nil
+	}
+
+	if len(blacklists) <= 0 {
+		return origin, nil
+	}
+
+	updated := origin
+	for i, p := range updated {
+		for _, blacklist := range blacklists {
+			// blacklist is not for current prize index
+			if prizeIndex <= blacklist.MaxPrizeIndex {
+				continue
+			}
+			for _, ID := range blacklist.IDs {
+				if p.ID == ID {
+					updated = append(updated[:i], updated[i+1:]...)
+				}
+			}
+		}
+	}
+	return updated, nil
+}
+
+func _getWinners(prizes []Prize, prizeIndex int, availables []Participant, blacklists []Blacklist) ([]Participant, error) {
+
+	prizesNum := len(prizes)
+	if prizesNum <= 0 {
+		return []Participant{}, fmt.Errorf("no prizes")
+	}
+
+	if prizeIndex < 0 || prizeIndex > prizesNum-1 {
+		return []Participant{}, fmt.Errorf("prize index error")
+	}
+
+	n := prizes[prizeIndex].Num
+	if n <= 0 {
+		return []Participant{}, fmt.Errorf("no prizes for prize index: %v\n", prizeIndex)
+	}
+
+	m := len(availables)
+	if m <= 0 {
+		return []Participant{}, fmt.Errorf("no participants")
+	}
+
+	updatedAvailables, err := getAvailParticipantsForPrize(prizeIndex, availables, blacklists)
+	if err != nil {
+		return []Participant{}, fmt.Errorf("failed to get available participants for prize index: %v\n", prizeIndex)
+	}
+
+	m = len(updatedAvailables)
+	if m <= 0 {
+		return []Participant{}, fmt.Errorf("no participants for prize index: %v\n", prizeIndex)
+	}
+
+	var winners []Participant
+	for i := 0; i < n; i++ {
+		rand.Seed(time.Now().UnixNano())
+		index := rand.Intn(m)
+		winners = append(winners, updatedAvailables[index])
+	}
+
+	return winners, nil
+}
---
+++ main.go
@@ -0,0 +1,58 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func main() {
+	var err error
+
+	flag.Parse()
+
+	if participants, err = loadParticipants(participantsCSV); err != nil {
+		fmt.Printf("loadParticipants() error: %v\n", err)
+		return
+	}
+
+	for _, p := range participants {
+		fmt.Printf("id: %v, name: %v\n", p.ID, p.Name)
+	}
+	availParticipants = participants
+
+	if err = loadConfig(configFile, &config); err != nil {
+		fmt.Printf("loadConfig() error: %v\n", err)
+		return
+	}
+	fmt.Printf("config: %v\n", config)
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+	err = http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
